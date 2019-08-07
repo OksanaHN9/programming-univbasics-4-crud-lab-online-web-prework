979263bada1c880b4fc4e249daf7dd7dbc77e5e6
Oksana Nichols <oksanahnichols@yahoo.com>
2019-08-07 03:04:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -52,7 +52,9 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+   arrays = ["wow", "I", "am", "really", "learning", "arrays!"]
+   
+   arrays [5]
 end
 
 def update_element_from_index(array, index_number, element)
