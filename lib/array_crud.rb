7b1440fb9909269db44fe9ef56d8a2c30d555e16
Oksana Nichols <oksanahnichols@yahoo.com>
2019-08-07 02:08:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  tea = ["green","black", ]
+  tea = ["green", "black", "jasmine", "earl grey"]
 end
 
 def add_element_to_end_of_array(array, element)
