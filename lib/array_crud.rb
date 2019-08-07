7706405408c2e59b4fcaee4e58ad881e0b73c6bc
Oksana Nichols <oksanahnichols@yahoo.com>
2019-08-07 03:00:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,10 +39,9 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  arrays = ["wow", "I", "am", "doing", ]
+  arrays = ["wow", "I", "am", "really", "learning", "arrays!"]
   
-  colors [2]
-  
+  arrays [2]
   
 end
 
