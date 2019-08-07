9fcb6266af63588e719b0272479b43c7b12e11a9
Oksana Nichols <oksanahnichols@yahoo.com>
2019-08-07 02:49:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  tea = ["green", "black", "jasmine","earl grey"]
+  tea = ["green", "black", "jasmine","earl grey", "arrays!"]
   
   earl_grey_tea = tea.pop 
   
