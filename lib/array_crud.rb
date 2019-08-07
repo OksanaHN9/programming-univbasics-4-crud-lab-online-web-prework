2dd919c147fd21e6836b689ddc9d99fb79069493
Oksana Nichols <oksanahnichols@yahoo.com>
2019-08-07 03:06:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -58,5 +58,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+ arrays = ["wow", "I", "am", "really", "learning", "arrays!"]
+ 
 end
